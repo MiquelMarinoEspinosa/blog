bdb37a71ccd030fc9c4b1d455824029e931d986b
Miquel Mariño Espinosa <miquel@MacBook-Air-de-Miquel.local>
2020-05-18 18:16:56 +0200

    custom directives

--- src/main.js
+++ src/main.js
@@ -3,6 +3,25 @@ import App from './App.vue'
 import axios from 'axios'
 
 Vue.prototype.$http = axios;
+Vue.directive('rainbow', {
+  bind(el){
+    el.style.color = "#" + Math.random().toString().slice(2,8);
+  }
+});
+
+Vue.directive('theme', {
+  bind(el, binding){
+    if (binding.value === 'wide') {
+      el.style.maxWidth = '1200px';
+    } else if (binding.value === 'narrow') {
+      el.style.maxWidth = '560px';
+    }
+    if (binding.arg === 'column') {
+      el.style.background = '#ddd';
+      el.style.padding = '20px';
+    }
+  }
+});
 
 Vue.config.productionTip = false
 
