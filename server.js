1ce192861b3147ba691d817d63bd6eaa08a76351
Miquel Mariño Espinosa <miquel@MacBook-Air-de-Miquel.local>
2020-05-31 17:05:26 +0200

    add body parser for post requests

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-var express = require("express");
-var app = express();
+var express = require("express")
+var bodyParser = require('body-parser')
+var app = express()
+app.use(bodyParser.json())
 
 app.listen(8081, () => {
- console.log("Server run port 8081");
+ console.log("Server run port 8081")
 });
 
 app.get('/blog', (req, res) => {
     var message = {
         'text': 'ola k ase?'
-    };
-    res.json(message);
+    }
+    res.json(message)
 });
 
 app.get('/blog/:id', (req, res) => {
-    res.json(req.params);
+    res.json(req.params)
 });
 
 app.post('/blog', (req, res) => {
-    console.log(req.body);
-    res.sendStatus(200);
+    res.sendStatus(200)
 });
