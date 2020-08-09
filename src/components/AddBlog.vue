c867119220f9897d41a18e55c1b6e9240f0b4f38
Miquel Mariño Espinosa <miquel@MacBook-Air-de-Miquel.local>
2020-08-09 18:52:09 +0200

    cors problem

--- src/components/AddBlog.vue
+++ src/components/AddBlog.vue
@@ -1,102 +1,104 @@
 <template>
   <div id="add-blog">
-      <h2>Add a New Blog Post</h2>
-      <form v-if="!submitted">
-          <label>Blog Title:</label>
-          <input type="text" required v-model.lazy="blog.title"/>
-          <label>Blog Content:</label>
-          <textarea v-model.lazy="blog.content"></textarea>
-      
+    <h2>Add a New Blog Post</h2>
+    <form v-if="!submitted">
+      <label>Blog Title:</label>
+      <input type="text" required v-model.lazy="blog.title" />
+      <label>Blog Content:</label>
+      <textarea v-model.lazy="blog.content"></textarea>
+
       <div id="checkboxes">
-          <label>Ninjas</label>
-          <input type="checkbox" value="ninjas" v-model="blog.categories"/>
-          <label>Wizards</label>
-          <input type="checkbox" value="wizards" v-model="blog.categories"/>
-          <label>Mario</label>
-          <input type="checkbox" value="mario" v-model="blog.categories"/>
-          <label>Cheese</label>
-          <input type="checkbox" value="cheese" v-model="blog.categories"/>
+        <label>Ninjas</label>
+        <input type="checkbox" value="ninjas" v-model="blog.categories" />
+        <label>Wizards</label>
+        <input type="checkbox" value="wizards" v-model="blog.categories" />
+        <label>Mario</label>
+        <input type="checkbox" value="mario" v-model="blog.categories" />
+        <label>Cheese</label>
+        <input type="checkbox" value="cheese" v-model="blog.categories" />
       </div>
       <label>Author:</label>
       <select v-model="blog.author">
-          <option v-for='(author, index) in authors' :key=index>{{ author }}</option>
+        <option v-for="(author, index) in authors" :key="index">{{
+          author
+        }}</option>
       </select>
       <button @click.prevent="post">Add Blog</button>
-      </form>
-      <div v-if="submitted">
-          <h3>Thanks for adding your post</h3>
-      </div>
-      <div id="preview">
-          <h3>Preview Blog</h3>
-          <p>Blog title: {{ blog.title }}</p>
-          <p>Blog content:</p>
-          <p>{{ blog.content }}</p>
-          <p>Blog categories:</p>
-          <ul>
-              <li v-for="(category, index) in blog.categories" :key="index">
-                  {{ category }}
-              </li>
-          </ul>
-          <p>Author: {{ blog.author }}</p>
-      </div>
-
+    </form>
+    <div v-if="submitted">
+      <h3>Thanks for adding your post</h3>
+    </div>
+    <div id="preview">
+      <h3>Preview Blog</h3>
+      <p>Blog title: {{ blog.title }}</p>
+      <p>Blog content:</p>
+      <p>{{ blog.content }}</p>
+      <p>Blog categories:</p>
+      <ul>
+        <li v-for="(category, index) in blog.categories" :key="index">
+          {{ category }}
+        </li>
+      </ul>
+      <p>Author: {{ blog.author }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            blog: {
-                title: '',
-                content: '',
-                categories: [],
-                author: ''
-            },
-            authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator'],
-            submitted: false
-        }
+  data() {
+    return {
+      blog: {
+        title: "",
+        content: "",
+        categories: [],
+        author: "",
+      },
+      authors: ["The Net Ninja", "The Angular Avenger", "The Vue Vindicator"],
+      submitted: false,
+    };
+  },
+  methods: {
+    post() {
+      this.$http.post("http://localhost:8081/blog", this.blog).then(() => {
+        this.submitted = true;
+      });
     },
-    methods: {
-        post() {
-            this.$http.post('https://create-blog.firebaseio.com/posts.json', this.blog).then(() => {
-                this.submitted = true;
-            });
-        }
-    }
-}
+  },
+};
 </script>
 
 <style>
-#add-blog *{
-    box-sizing: border-box;
+#add-blog * {
+  box-sizing: border-box;
 }
-#add-blog{
-    margin: 20px auto;
-    max-width: 500px;
+#add-blog {
+  margin: 20px auto;
+  max-width: 500px;
 }
-label{
-    display: block;
-    margin: 20px 0 10px;
+label {
+  display: block;
+  margin: 20px 0 10px;
 }
-input[type="text"], textarea{
-    display: block;
-    width: 100%;
-    padding: 8px;
+input[type="text"],
+textarea {
+  display: block;
+  width: 100%;
+  padding: 8px;
 }
-#preview{
-    padding: 10px 20px;
-    border: 1px dotted #ccc;
-    margin: 30px 0;
+#preview {
+  padding: 10px 20px;
+  border: 1px dotted #ccc;
+  margin: 30px 0;
 }
-h3{
-    margin-top: 10px;
+h3 {
+  margin-top: 10px;
 }
 #checkboxes input {
-    display: inline-block;
-    margin-right: 10px;
+  display: inline-block;
+  margin-right: 10px;
 }
 #checkboxes label {
-    display: inline-block;
+  display: inline-block;
 }
 </style>
