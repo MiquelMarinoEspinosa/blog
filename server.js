4b43bf0354e42fcce4957641fcdafc7f0413828f
Miquel Mariño Espinosa <miquel@MacBook-Air-de-Miquel.local>
2020-08-09 18:22:14 +0200

    use async to get and post to redis

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
-const redis = require("redis")
-const client = redis.createClient(6379, 'blog.redis_server')
+const redis = require("redis");
+const client = redis.createClient(6379, "blog.redis_server");
+const { v4: uuidv4 } = require("uuid");
 
-var express = require("express")
-var bodyParser = require('body-parser')
+client.on("error", function(error) {
+  console.error(error);
+});
 
-var app = express()
-app.use(bodyParser.json())
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(bodyParser.json());
 
 app.listen(8081, () => {
- console.log("Server run port 8081")
+  console.log("Server run port 8081");
 });
 
-app.get('/blog', (req, res) => {
-    var message = {
-        'text': 'ola k ase?'
+app.get("/blog/:id", (req, res) => {
+  const blog = client.get("blog_" + req.params.id, function(err, response) {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: "Service not available", message: err });
     }
-    res.json(message)
+
+    return response === null
+      ? res.status(404).json({ error: "Blog not found" })
+      : res.status(200).json(response);
+  });
 });
 
-app.get('/blog/:id', (req, res) => {
-    res.json(req.params)
-});
-
-app.post('/blog', (req, res) => {
-    client.set('blog_' + req.body.id, JSON.stringify(req.body));
-    res.sendStatus(200)
+app.post("/blog", (req, res) => {
+  const id = uuidv4();
+  client.set("blog_" + id, JSON.stringify({ ...req.body, id: id }), function(
+    err,
+    response
+  ) {
+    res.status(201).send("");
+  });
 });
