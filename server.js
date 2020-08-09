d0e2b94ba40c9c2bab4ab7d4da7e7ae37b6a75d0
Miquel Mariño Espinosa <miquel@MacBook-Air-de-Miquel.local>
2020-08-09 18:37:46 +0200

    use redis sets instead of individual keys

--- server.js
+++ server.js
@@ -16,23 +16,36 @@ app.listen(8081, () => {
   console.log("Server run port 8081");
 });
 
-app.get("/blog/:id", (req, res) => {
-  const blog = client.get("blog_" + req.params.id, function(err, response) {
+app.get("/blog", (req, res) => {
+  const blogs = client.smembers("blogs", function(err, response) {
     if (err) {
       return res
         .status(500)
         .json({ error: "Service not available", message: err });
     }
 
-    return response === null
-      ? res.status(404).json({ error: "Blog not found" })
-      : res.status(200).json(response);
+    return response ? res.status(200).json(response) : res.status(200).json([]);
+  });
+});
+
+app.get("/blog/:id", (req, res) => {
+  const blog = client.smembers("blogs", function(err, response) {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: "Service not available", message: err });
+    }
+
+    const blog = response.find((blog) => JSON.parse(blog).id === req.params.id);
+    return blog
+      ? res.status(200).json(blog)
+      : res.status(404).json({ error: "Blog not found" });
   });
 });
 
 app.post("/blog", (req, res) => {
   const id = uuidv4();
-  client.set("blog_" + id, JSON.stringify({ ...req.body, id: id }), function(
+  client.sadd("blogs", JSON.stringify({ ...req.body, id: id }), function(
     err,
     response
   ) {
