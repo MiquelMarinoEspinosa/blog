0e8997fd4a028921109ddf0ecf228787c7bf715a
Miquel Mariño Espinosa <miquel@MacBook-Air-de-Miquel.local>
2020-08-17 18:08:22 +0200

    unit test routes and complete unit test

--- tests/unit/routes.spec.js
+++ tests/unit/routes.spec.js
@@ -1,28 +1,62 @@
-import { mount, createLocalVue, shallowMount } from "@vue/test-utils";
-import VueRouter from "vue-router";
+import { mount, createLocalVue } from "@vue/test-utils";
+import Router from "vue-router";
 import routes from "@/routes";
 import App from "@/App";
 import ShowBlogs from "@/components/ShowBlogs";
 import AddBlog from "@/components/AddBlog";
+import Header from "@/components/Header";
+import SingleBlog from "@/components/SingleBlog";
+import axios from "axios";
 
 const localVue = createLocalVue();
 
-localVue.use(VueRouter);
+localVue.use(Router);
+jest.mock("axios");
+localVue.prototype.$http = axios;
 
 describe("router", () => {
+  let router;
+  let app;
+  beforeAll(() => {
+    const data = [];
+    router = new Router({ routes });
+    axios.get.mockResolvedValue({ data });
+    app = mount(App, {
+      localVue,
+      router,
+      stubs: { "app-header": Header },
+    });
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("path: /, component: ShowBlogs", async () => {
-    const router = new VueRouter({ routes });
-    const app = mount(App, { localVue, router });
     router.push("/");
     await localVue.nextTick();
     expect(app.find(ShowBlogs).exists()).toBeTruthy();
   });
 
   it("path: /add, component: AddBlog", async () => {
-    const router = new VueRouter({ routes });
-    const app = mount(App, { localVue, router });
     router.push("/add");
     await localVue.nextTick();
-    expect(app.find(ShowBlogs).exists()).toBeTruthy();
+    expect(app.find(AddBlog).exists()).toBeTruthy();
+  });
+
+  it("path: /blog/:id, component: SingleBlog", async () => {
+    const data = {
+      title: "Blog 1",
+      content: "Blog content 1",
+      author: "Author 1",
+      categories: ["vue", "react", "node"],
+    };
+    const id = "e2e6d160-15ff-4d30-92ce-2799eb7fe5b";
+    axios.get.mockResolvedValue({
+      data,
+    });
+    router.push("/blog/" + id);
+    await localVue.nextTick();
+    expect(app.find(SingleBlog).exists()).toBeTruthy();
   });
 });
